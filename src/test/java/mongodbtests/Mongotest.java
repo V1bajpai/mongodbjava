b5cd211ff51a756d3d58386bcbae61389e711363
Vaibhav bajpai <vaibhavbajpai07059@gmail.com>
2021-10-19 13:03:16 +0530

    select, insert

--- src/test/java/mongodbtests/Mongotest.java
+++ src/test/java/mongodbtests/Mongotest.java
@@ -1,11 +1,13 @@
 package mongodbtests;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.bson.Document;
 
 import com.mongodb.*;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -24,23 +26,25 @@ public class Mongotest {
 		
 		// Creating a collection
 //		database.createCollection("sampleCollection");
-		System.out.println("Collection created successfully");
+//		System.out.println("Collection created successfully");
 		
 		// Retrieving a collection
 		MongoCollection<Document> collection = database.getCollection("employees");
-		System.out.println("Collection sampleCollection selected successfully");
-		
-		//Single document insertion
-//		Document document = new Document("title", "MongoDB")
-//		.append("description", "database")
-//		.append("likes", 100)
-//		.append("url", "http://www.tutorialspoint.com/mongodb/")
-//		.append("by", "tutorials point");
-		
-//       Inserting document into the collection
-
-//		collection.insertOne(document1);
-		
+		System.out.println("Collection sampleCollection selected successfully");	
+		insertSingleIntoDatabase(collection); // call fun insertSingleIntoDatabase
+	    selectAllRecordsFromACollection(collection);  // call fun selectAllRecordsFromACollection
+	}
+	
+	// Single node insertion
+	public static void insertSingleIntoDatabase(MongoCollection collection) {
+		Document document1 = new Document("name", "Peterson")
+		.append("age", 22)
+		.append("city", "Kabul")
+		.append("salary", 22000);
+		collection.insertOne(document1);   //       Inserting document into the collection
+	}
+	
+	public void insertManyIntoDatabase(MongoCollection collection) {
 		  Document document1 = new Document("name", "Ram").append("age", 26).append("city", "Hyderabad").append("salary", 10000);
 	      Document document2 = new Document("name", "Robert").append("age", 27).append("city", "Vishakhapatnam").append("salary", 11000);
 	      Document document3 = new Document("name", "Gonna").append("age", 30).append("city", "Delhi").append("salary", 12000);
@@ -51,6 +55,7 @@ public class Mongotest {
 	      Document document8 = new Document("name", "Darvin").append("age", 30).append("city", "Noida").append("salary", 17000);
 	      Document document9 = new Document("name", "Larry").append("age", 30).append("city", "Gurugram").append("salary", 18000);
 	      Document document10 = new Document("name", "Mack").append("age", 30).append("city", "Agra").append("salary", 19000);
+		  Document document11 = new Document("name", "Robin").append("age", 23).append("city", "London").append("salary", 23000);
 
 	      //Inserting the created documents
 	      List<Document> list = new ArrayList<Document>();
@@ -64,21 +69,19 @@ public class Mongotest {
 	      list.add(document8);
 	      list.add(document9);
 	      list.add(document10);
+		  list.add(document11);
 
-	      collection.insertMany(list);
-
-		
-	      
-	      // select all
-		  DBCollection collection1 = (DBCollection) database.getCollection("employees");
-
-	      System.out.println("\n1. Find all matched documents");
-		  DBCursor cursor = collection1.find();
-		  while (cursor.hasNext()) {
-			System.out.println(cursor.next());
-		  }
-		System.out.println("Document inserted successfully");
-
+	      collection.insertMany(list);  // Insert many
+		  System.out.println("Document inserted successfully");
+	}
+	
+	private static void selectAllRecordsFromACollection(MongoCollection collection) 
+	{
+		  FindIterable<Document> iterDoc = collection.find();
+	      Iterator it = iterDoc.iterator();
+	      while (it.hasNext()) {
+	         System.out.println(it.next());
+	      }
 	}
 
 }
